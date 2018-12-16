9acde23684b0b0c22b881f0a21dd62b65dc80e11
Ethan Liang <ethanl0518@gmail.com>
2018-12-16 11:11:22 +0800

    none

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
-
 import sys, random
 import oauth2 as oauth
+from time import sleep
 from plurk_oauth import PlurkAPI
+import time 
+
 
 key = "bpahuUGTawKW"
 secret = "fiXYi9wmXqGNaKierCDdxBgjgxYv9JDv"
@@ -12,4 +14,7 @@ plurk = PlurkAPI(key, secret)
 plurk.authorize(token, token_secret)
 
 
-plurk.callAPI("/APP/Timeline/plurkAdd", options={"content": str(random.random()), "qualifier": "is", "limited_to": "[14129109, 11613769, 13734980]"})
+p = plurk.callAPI("/APP/Timeline/plurkAdd", options={"content": str(random.random()), "qualifier": "is", "limited_to": "[14129109, 11613769, 13734980]"})
+sleep(1)
+plurk.callAPI("/APP/Timeline/plurkDelete", options={"plurk_id": str(p["plurk_id"])})
+print (time.strftime("%Y/%m/%d %H:%M:%S"))
